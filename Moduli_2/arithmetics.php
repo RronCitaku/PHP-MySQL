4ffcee937b391ff597987d11feaedceebae00353
RronCitaku <citakurron@gmail.com>
2026-02-16 20:28:35 +0100

    mod2

---
+++ Moduli_2/arithmetics.php
@@ -0,0 +1,11 @@
+<?php
+    $x = 120;
+    $y = 50;
+
+    // echo $x - $y;
+    // echo $x + $y;
+    // echo $x * $y;
+    // echo $x / $y;
+    echo $x % $y;
+
+?>
---
+++ Moduli_2/concatenate_strings.php
@@ -0,0 +1,10 @@
+<?php
+
+    $a = 'Digital';
+    $b = 'School';
+
+    $c = $a.$b;
+
+    echo "$c \n"
+
+?>
