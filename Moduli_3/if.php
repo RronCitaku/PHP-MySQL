dac5a43bd5b802a634a5873ab90e62d1cdbcd43a
RronCitaku <citakurron@gmail.com>
2026-02-18 20:47:10 +0100

    moduli3

---
+++ Moduli_3/if.php
@@ -0,0 +1,52 @@
+<?php
+    // $num = 5;
+    // if($num <= 0){
+    //     echo "$num is less than 0";
+    
+    // $age = 15;
+    // if($age < 18){
+    //     echo "You are underage";
+    // }else{
+    //     echo "You are an adult";
+    // }
+
+    // $age = 15;
+    // if(($age > 12) && ($age < 18)){
+    //     echo "You are a teenager";
+    // }
+
+    // $num = 5;
+    // if($num < 0){
+    //     echo "The value of $num is a negative number";
+    // }elseif($num == 0){
+    //     echo "The value of $num is 0";
+    // }else{
+    //     echo "The value of $num is a positive number";
+    // }
+
+    //Exercise1: Output yes if a = b, otherwise output no'
+    //Exercise1: Output 1 if c = d, print 2 if c > d, otherwise output 3'
+
+
+    //Exercise1:
+    $a = 3;
+    $b = 4;
+    if($a == $b){
+        echo "yes";
+    }else{
+        echo "no";
+    }
+
+
+    //Exercise2:
+    $c = 2;
+    $d = 3;
+    if($c == $d){
+        echo "1";
+    }elseif($c > $d){
+        echo "2";
+    }else{
+        echo "3";
+    }
+
+?>
